5e749baa42db5ae8f6b85a7bd9f9557c4f9c6c4f
yaveyave <danielyanez981@outlook.com>
2025-07-01 10:03:47 -0500

    Solved 404 error

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
-  i18n: {
-    locales: ['en', 'es'],
-    defaultLocale: 'en'
-  }
+  reactStrictMode: true,
+  // sin i18n
 };
 
 module.exports = nextConfig;
